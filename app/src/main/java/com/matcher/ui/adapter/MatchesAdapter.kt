c0a916e3b4ec3865dc10c7a4cae9f47a5ee9ab3b
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-06-23 18:34:53 -0300

    add glide and insert team images into layout

--- app/src/main/java/com/matcher/ui/adapter/MatchesAdapter.kt
+++ app/src/main/java/com/matcher/ui/adapter/MatchesAdapter.kt
@@ -3,6 +3,7 @@ package com.matcher.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.matcher.databinding.MatchItemBinding
 import com.matcher.domain.Match
 
@@ -27,8 +28,12 @@ class MatchesAdapter(private val matches: List<Match>):
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val context = viewHolder.itemView.context
         val match = matches[position]
+
+        Glide.with(context).load(match.firstTeam.image).circleCrop().into(viewHolder.binding.ivFirstTeamImage)
         viewHolder.binding.tvFirstTeamName.text = match.firstTeam.name
+        Glide.with(context).load(match.secondTeam.image).circleCrop().into(viewHolder.binding.ivSecondTeamImage)
         viewHolder.binding.tvSecondTeamName.text = match.secondTeam.name
     }
 
