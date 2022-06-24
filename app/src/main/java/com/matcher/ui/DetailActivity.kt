6e526b9076e0c3fe50129a3f257e50a5716ee68d
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-06-23 21:11:53 -0300

    render data from extras into details layout

--- app/src/main/java/com/matcher/ui/DetailActivity.kt
+++ app/src/main/java/com/matcher/ui/DetailActivity.kt
@@ -2,7 +2,9 @@ package com.matcher.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.matcher.databinding.ActivityDetailBinding
+import com.matcher.domain.Match
 
 class DetailActivity : AppCompatActivity() {
     object Extras {
@@ -19,5 +21,30 @@ class DetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loadMatchFromExtra()
+    }
+
+    private fun loadMatchFromExtra() {
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let { match ->
+            supportActionBar?.title = match.place.name
+            Glide.with(this).load(match.place.image).into(binding.ivPlace)
+            binding.tvDescription.text = match.description
+
+            Glide.with(this).load(match.firstTeam.image).circleCrop().into(binding.ivFirstTeamImage)
+            binding.tvFirstTeamName.text = match.firstTeam.name
+            binding.rbFirstTeamStars.rating = match.firstTeam.stars.toFloat()
+            if (match.firstTeam.score != null) {
+                binding.tvFirstTeamGoals.text = match.firstTeam.score.toString()
+
+            }
+
+            Glide.with(this).load(match.secondTeam.image).circleCrop().into(binding.ivSecondTeamImage)
+            binding.tvSecondTeamName.text = match.secondTeam.name
+            binding.rbSecondTeamStars.rating = match.secondTeam.stars.toFloat()
+            if (match.secondTeam.score != null) {
+                binding.tvSecondTeamGoals.text = match.secondTeam.score.toString()
+            }
+        }
     }
 }
